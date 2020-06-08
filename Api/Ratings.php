202e162e1b91885c559f8e341accdd1699af879b
saravana128 <saravana.a128@gmail.com>
2020-06-08 20:44:29 +0530

    close

--- Api/Ratings.php
+++ Api/Ratings.php
@@ -7,23 +7,28 @@ if (!$con) {
 }
 
 mysqli_select_db($con,"ajax_demo");
-$sql="SELECT vote FROM charecter WHERE id = 101";
+$sql="SELECT vote FROM charecter WHERE ID = 101";
 $yes = mysqli_query($con,$sql);
-$sql2="SELECT vote FROM charecter WHERE id = 102";
+$sql2="SELECT vote FROM charecter WHERE ID = 102";
 $no = mysqli_query($con,$sql2);
+
+echo($yes);
+echo(" "+$no);
 
 if($vote = "101")
 {
 	$yes = $yes +1;
-	$sql3="UPDATE charecter SET vote = $yes WHERE id = 101";
+	$sql3="UPDATE charecter SET vote = $yes WHERE ID = 101";
 	mysqli_query($con,$sql3);
 }
 else
 {
 	$no =$no+1;
-	$sql3="UPDATE charecter SET vote = $no WHERE id = 102";
+	$sql3="UPDATE charecter SET vote = $no WHERE ID = 102";
 	mysqli_query($con,$sql3);
 }
+
+mysqli_close($con);
 
 echo(100*round($yes/($no+$yes),2));
 echo(" ");
